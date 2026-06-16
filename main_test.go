c7b09c763f157a299602e1959ffddfcc68902baa
elvictoraso <victorabelra@gmail.com>
2026-06-16 10:14:28 -0600

    correcion main.test

--- main_test.go
+++ main_test.go
@@ -2,21 +2,23 @@ package main
 
 import "testing"
 
-func TestCheckPasswordStrength(t *testing.T) {
+func TestAnalyzePasswordStrength(t *testing.T) {
 	tests := []struct {
 		password      string
 		expectedLevel string
 	}{
-		{"123", "Baja"},                  // Muy corta
+		{"123", "Muy Baja"},               // Muy corta (< 6 caracteres)
 		{"password123", "Baja"},           // Larga pero sin mayúsculas/especiales
-		{"Pass123a", "Media"},            // MODIFICADO: longitud 8 y cumple 3 criterios
-		{"Segura123$Longitud", "Alta"},    // Robusta, >12 caracteres y todos los criterios
+		{"Pass123a", "Media"},             // Longitud 8 y cumple 3 criterios
+		{"Segura123$Longitud", "Alta"},    // Robusta, >= 12 caracteres y 4+ criterios
 	}
 
 	for _, tt := range tests {
-		level, _ := CheckPasswordStrength(tt.password)
-		if level != tt.expectedLevel {
-			t.Errorf("Para '%s' se esperaba nivel %s, pero se obtuvo %s", tt.password, tt.expectedLevel, level)
+		// Llamamos a la nueva función
+		response := AnalyzePasswordStrength(tt.password)
+		
+		if response.Level != tt.expectedLevel {
+			t.Errorf("Para '%s' se esperaba nivel %s, pero se obtuvo %s", tt.password, tt.expectedLevel, response.Level)
 		}
 	}
 }
